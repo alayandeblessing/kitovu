5c6be8d0bf9a2f2e32d7680c7e071e81d32786fb
ndukwesamuel23 <ndukwesamuel23@gmail.com>
2020-10-15 15:54:00 +0100

    my first version

--- kitovuPK/settings.py
+++ kitovuPK/settings.py
@@ -132,6 +132,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
